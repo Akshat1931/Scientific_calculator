743639a987b4206844b279755225979c976af2e8
Akshat Baranwal <141770012+Akshat1931@users.noreply.github.com>
2024-04-14 09:47:34 +0000

    implemented few scientific function & created a seperate scripe file

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const display = document.getElementById('calc-display');
+    const buttons = document.getElementsByClassName('btn');
+    let currentValue="";
+  
+    function evaluateResult(){
+      const convertedValue = currentValue.replace("×","*").replace("÷","/").replace("%","*0.01").replace('sin', 'Math.sin').replace('cos', 'Math.cos').replace('π', 'Math.PI').replace('ln', 'Math.log').replace('log', 'Math.log10').replace('e', 'Math.E').replace('tan', 'Math.tan').replace('√', 'Math.sqrt');
+      const result = eval(convertedValue);
+      currentValue=result.toString();
+      display.value=currentValue;
+    }
+    for(let i=0;i<buttons.length; i++){
+      const button=buttons[i];
+      button.addEventListener('click',function(){
+        const value=button.innerText;
+        if (value=="AC"){
+          currentValue="";
+          display.value=currentValue;
+        }
+        else if(value == "="){
+          evaluateResult();
+        }
+        else{
+        currentValue+=value;
+        display.value=currentValue;
+        }
+      })
+    }
+    });
